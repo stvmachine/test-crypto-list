ce10bd8e955bf9b8882bd162c128cf632094392e
stevmachine <campos.esteban@gmail.com>
2018-07-07 21:53:23 +1000

    using redux on Home

--- src/routes/components/Home.js
+++ src/routes/components/Home.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { getSymbols } from '../../api';
+import { loadTickers } from '../../actions';
 
 class Home extends React.Component {
-  state = {
-    symbols: [],
-  };
+  constructor() {
+    super();
+    this.state = {
+      tickers: [],
+    };
+  }
 
-  componentDidMount() {
-    getSymbols().then(symbols => {
-      this.setState({ symbols });
-    });
+  componentWillMount() {
+    const { tickers, _loadTickers } = this.props;
+    if (tickers.length === 0) {
+      getSymbols().then((incomingTickers) => {
+        this.setState({ tickers: incomingTickers });
+        _loadTickers(incomingTickers);
+      });
+    } else {
+      this.setState({ tickers });
+    }
   }
 
   render() {
-    const { symbols } = this.state;
+    const { tickers } = this.state;
     return (
       <div>
-        <h2>Home</h2>
+        <h2>
+Home
+        </h2>
         <div>
           <ul>
-            {symbols.map(n => (
+            {tickers.map(n => (
               <li key={n}>
-                <Link to={`detail/${n}`}>{n}</Link>
+                <Link to={`detail/${n}`}>
+                  {n}
+                </Link>
               </li>
             ))}
           </ul>
@@ -32,4 +48,20 @@ class Home extends React.Component {
   }
 }
 
-export default Home;
+const mapStateToProps = state => ({
+  tickers: state.fetching.tickers ? state.fetching.tickers : [],
+});
+
+const mapDispatchToProps = {
+  _loadTickers: loadTickers,
+};
+
+Home.propTypes = {
+  _loadTickers: PropTypes.func.isRequired,
+  tickers: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Home);
