b0871ac11167d41810cc6239397d7c99c09fee1e
stevmachine <campos.esteban@gmail.com>
2018-07-07 10:54:41 +1000

    restructuring routes

---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Header from './components/Header';
+import Content from './components/Content';
+import Footer from './components/Footer';
+import withRoot from '../withRoot';
+
+const styles = {
+  root: {
+    display: 'flex',
+    minHeight: '100vh',
+    flexDirection: 'column',
+  },
+};
+
+const Routes = ({ classes }) => (
+  <div className={classes.root}>
+    <Header />
+    <Content />
+    <Footer />
+  </div>
+);
+
+Routes.propTypes = {
+  classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+
+export default withRoot(withStyles(styles)(Routes));
