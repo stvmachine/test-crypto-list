1be1adb500bdba1684a1a489b0323fec57f9028d
stevmachine <campos.esteban@gmail.com>
2018-07-08 09:03:03 +1000

    added support with localStorage

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,6 @@ module.exports = {
   },
   globals: {
     document: 1,
+    window: true,
   },
 };
--- src/configApp.js
+++ src/configApp.js
@@ -3,13 +3,26 @@ import { connectRouter, routerMiddleware } from 'connected-react-router';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { createBrowserHistory } from 'history';
+import throttle from 'lodash/throttle';
 import reducers from './reducers';
+import { loadState, saveState } from './localStorage';
 
+const persistedState = loadState();
 const history = createBrowserHistory();
 const middlewares = [thunk, routerMiddleware(history), createLogger()];
 const store = createStore(
   connectRouter(history)(reducers),
+  persistedState,
   compose(applyMiddleware(...middlewares)),
+);
+
+/* Save state changes in localStorage */
+store.subscribe(
+  throttle(() => {
+    saveState({
+      fetching: store.getState().fetching,
+    });
+  }, 1000),
 );
 
 export { store, history };
